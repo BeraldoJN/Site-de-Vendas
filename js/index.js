30e2d8f204aef92115e9b702eea5a541f66f0873
BeraldoJN <josenetoberaldo@gmail.com>
2025-08-26 09:00:08 -0300

    Correção

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
-function next(){
-    document.getElementById('prev').hidden = true;
-    document.getElementById('next').hidden = false;
+function next(){ //carrosel
+    document.getElementById('prev').hidden = true; //esconde a imagem principal
+    document.getElementById('next').hidden = false; //mostra a secunda imagem
     
 }
 
 function prev(){
-    document.getElementById('next').hidden = true;
-    document.getElementById('prev').hidden = false;
+    document.getElementById('next').hidden = true; //mostra a primeira imagem
+    document.getElementById('prev').hidden = false; //esconde a primeira imagem
 }
+
+setInterval(next, 15000);
+setInterval(prev, 30000)
+
